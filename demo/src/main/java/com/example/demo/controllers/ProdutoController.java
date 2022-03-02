c5f36b6b11ac7fc969e58b1af6465fc8feeb72a4
João Victor Alves <77747613+joaoalves-dev@users.noreply.github.com>
2022-03-02 16:57:17 -0300

    CRUD completo
    Adição do crud completo

--- demo/src/main/java/com/example/demo/controllers/ProdutoController.java
+++ demo/src/main/java/com/example/demo/controllers/ProdutoController.java
@@ -1,13 +1,15 @@
 package com.example.demo.controllers;
 
-import java.util.List;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,15 +23,36 @@ public class ProdutoController {
 	@Autowired
 	private ProdutoRepository produtoRepository;
 	
-	@PostMapping
-	public @ResponseBody Produto novoProduto(@Valid Produto produto) {		
+	//CREATE AND UPDATE
+	@RequestMapping(method = {RequestMethod.POST, RequestMethod.PUT})
+	public @ResponseBody Produto salvarProduto(@Valid Produto produto) {		
 		produtoRepository.save(produto);
 		return produto;
 	}
 	
-	@GetMapping("/list")
-	public List<Produto> list(){
-		return (List<Produto>) this.produtoRepository.findAll();
+	//READ
+	@GetMapping
+	public Iterable<Produto> obterProduto(){
+		return produtoRepository.findAll();
 	}
+	
+	//READ BY ID
+	@GetMapping(path = "/{id}")
+	public Optional<Produto> obterProdutoPorId(@PathVariable int id) {
+		return produtoRepository.findById(id);
+	}
+	
+	//DELETE
+	@DeleteMapping(path = "/{id}")
+	public void excluirProduto(@PathVariable int id) {
+		produtoRepository.deleteById(id);
+	}
+	
+//	//UPDATE
+//	@PutMapping
+//	public Produto alterarProduto(@Valid Produto produto) {
+//		produtoRepository.save(produto);
+//		return produto;
+//	}
 
 }
